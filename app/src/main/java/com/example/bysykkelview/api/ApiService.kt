ad2dcbb5bc12ce948af61ffa5527d1d6b6d5e186
Håkon Gimse <hakongimse@gmail.com>
2021-11-08 04:06:01 +0100

    add client identifier + style readme

--- app/src/main/java/com/example/bysykkelview/api/ApiService.kt
+++ app/src/main/java/com/example/bysykkelview/api/ApiService.kt
@@ -4,12 +4,17 @@ import com.example.bysykkelview.models.StationInformationRoot
 import com.example.bysykkelview.models.StationStatusRoot
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
+
+const val clientIdentifier: String = "f12f145f-5c9f-4b8e-9a02-7930b5f3f904"
 
 interface ApiService {
 
+    @Headers("Client-Identifier: $clientIdentifier")
     @GET("station_information.json")
     fun getStationInformation(): Call<StationInformationRoot>
 
+    @Headers("Client-Identifier: $clientIdentifier")
     @GET("station_status.json")
     fun getStationStatus(): Call<StationStatusRoot>
 }
